d663bab64157dc836dec3d8aca198c1ecc20a949
Saad <isaadibrahim2005@gmail.com>
2025-04-22 00:48:10 +0500

    Added user info api

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js'
-
+import userRouter from './routes/userRoutes.js';
 
 const app = express()
 const port = process.env.PORT || 4000
-connectDB();                                           
+connectDB();
 
-
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials:true}));
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-
-app.get('/', (req,res)=>res.send("Hello world"))
-app.use('/api/auth',authRouter)
+// Routes
+app.get('/', (req,res)=>res.send("Hello world"));
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 
 app.listen(port,()=>console.log(`Server started on ${port}`))
 
