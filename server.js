6643288524f7cc2a28dbed8c35ebffcaf8cff8b4
Saad <isaadibrahim2005@gmail.com>
2025-04-28 03:36:23 +0500

    Added rooms

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ import callRouter from './routes/callRouter.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import userModel from './models/userModel.js';
+import socket from '../socket_learn/client/frontend/src/socket.js';
+import { send } from 'vite';
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors:{
+    origin: ["http://localhost:8080", "http://localhost:8081"],
+    methods: ["GET", "POST"],
+    credentials:true
+}
+})
 
 
 const port = process.env.PORT || 4000
@@ -22,10 +30,9 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:8080',
-  credentials: true,
+  origin: ["http://localhost:8080", "http://localhost:8081"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 // Routes
@@ -33,6 +40,8 @@ app.get('/', (req,res)=>res.send("Hello world"));
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 app.use('/api/call', callRouter);
+
+// remove in production
 app.get("/updateDb",async (req,res)=>{
   const result = await userModel.updateMany(
     {}, // Match all documents
@@ -49,8 +58,52 @@ app.get("/updateDb",async (req,res)=>{
 
 })
 
+
+let getRoomId = (sender, receiver)=>{
+
+  return [sender,receiver].sort().join("-");
+
+}
+
+io.on('connection', (socket)=>{
+
+  console.log('client connected', socket.id);
+  
+  socket.on('joinRoom', async (data)=>{
+
+    let senderMail = data.sender;
+    let receiverMail = data.receiver;
+    
+    const sender = await userModel.findOne({email: senderMail})
+    const receiver = await userModel.findOne({email: receiverMail})
+
+    let roomId = getRoomId(sender._id.toString(), receiver._id.toString());
+
+    socket.join(roomId);
+    console.log(`${sender._id.toString()} joined room ${roomId}`);
+    
+
+  })
+
+  socket.on("sendMessage", async (data)=>{
+
+    const sender = await userModel.findOne({email: data.sender})
+    const receiver = await userModel.findOne({email: data.receiver})
+
+    let message = data.message;
+
+    let roomId = getRoomId(sender._id.toString(), receiver._id.toString())
+
+    io.to(roomId).emit("receiveMessage", message)
+    console.log("MEssage emitted");
+    
+
+  })
+
+});
+
+
 server.listen(port,()=>console.log(`Server started on ${port}`))
-export default io;
 
 
 
