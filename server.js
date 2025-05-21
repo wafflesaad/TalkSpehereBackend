d60bad275fee9148288fa72e5f9c54a75afc9770
Muhammad Saad Ibrahim <isaadibrahim2005@gmail.com>
2025-05-21 07:43:52 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
   cors:{
-    origin: ["http://localhost:8080", "http://localhost:8081"],
+    origin: ["http://localhost:8080", "http://localhost:8081", "https://talk-sphere-frontend-green.vercel.app"],
     methods: ["GET", "POST"],
     credentials:true
 }
