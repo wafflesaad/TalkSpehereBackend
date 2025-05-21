c77a468d3abb30c1163307859201e53bc854af78
Muhammad Saad Ibrahim <isaadibrahim2005@gmail.com>
2025-05-21 07:48:09 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ export const register = async (req,res)=>{
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'development',
-            sameSite: process.env.NODE_ENV === 'production'? 'none': 'strict',
+            sameSite: 'none',
             maxAge: 1*60*60*1000,
         });
 
@@ -82,7 +82,7 @@ export const login = async (req,res)=>{
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'development',
-            sameSite: process.env.NODE_ENV === 'production'? 'none': 'strict',
+            sameSite: 'none',
             maxAge: 1*60*60*1000,
         });
 
